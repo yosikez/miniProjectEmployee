119928e5fa4885a5b890fdad0f6ee2768a9a1f9f
yosikez <yosikez@gmail.com>
2023-02-28 13:10:30 +0700

    route

--- router/api.go
+++ router/api.go
@@ -11,9 +11,9 @@ func RegisterRouter(router *gin.Engine){
 	employeeController := controller.NewEmployeeController()
 
 	router.GET("/employees", employeeController.FindAll)
-	router.GET("/employee/:id", employeeController.FindById)
-	router.POST("/employee", employeeController.Create)
-	router.PUT("/employee/:id", employeeController.Update)
-	router.DELETE("/employee/:id", employeeController.Delete)
+	router.GET("/employees/:id", employeeController.FindById)
+	router.POST("/employees", employeeController.Create)
+	router.PUT("/employees/:id", employeeController.Update)
+	router.DELETE("/employees/:id", employeeController.Delete)
 
 }
